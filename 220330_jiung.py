a761abffb5a4cec1d4f2f617eda81b1977f9834a
JiungChoi <wldnd6091@gmail.com>
2022-03-30 12:27:51 +0900

    Create 220330_jiung.py

---
+++ 220330_jiung.py
@@ -0,0 +1,28 @@
+# 떡복이 떡 만들기
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+dduckAry = sorted(list(map(int, sys.stdin.readline().split())))
+
+def dduck(ary, M):
+
+    st = 0
+    end = ary[-1]
+    
+    while(st<end):
+        H = (end+st)//2
+        total = 0
+        for idx in range(len(ary)):
+            total += (ary[idx]-H) if ary[idx] > H else 0
+        
+        if total > M : st = H+1
+        elif total < M : end = H-1
+        else: return H
+    return H+1
+
+print(dduck(dduckAry, M))
+
+
+
+        
